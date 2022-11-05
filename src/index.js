b995290036ff6a2e491a6b8ce2b3c7ab0899a41e
Matteo <mrtn.matteo@protonmail.com>
2022-11-06 00:51:21 +0100

    shuffle card names

--- src/index.js
+++ src/index.js
@@ -4,32 +4,45 @@ function getRandomCard() {
   return axios.get('https://api.scryfall.com/cards/random?q=' + query)
 }
 
-let get = true
+function creaBottoni() {
+  const nodes = []
+  for (let i = 0; i < 4; i++) {
+    const node = document.createElement('button')
+    node.classList.add('bg-blue-500', 'hover:bg-blue-400', 'text-white', 'font-bold', 'py-2', 'px-4', 'rounded', 'w-auto')
+    nodes.push(node)    
+  }
+  return nodes
+}
 
-if (get) {
-  Promise.all([getRandomCard(), getRandomCard(), getRandomCard(), getRandomCard()])
+function shuffleArray(array) {
+  const newArray = [...array];
+  for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+  }
+  return newArray
+}
+
+Promise.all([getRandomCard(), getRandomCard(), getRandomCard(), getRandomCard()])
   .then(
-    r => {
-      console.log(r)
-      let carta = document.querySelector("#immagine-carta")
-      axios.get(`https://api.scryfall.com/cards/${r[0].data.set}/${r[0].data.collector_number}`).then(
-        e => {
-          console.log(e)
-          carta.style.backgroundImage = `url(${e.data.image_uris.art_crop})`;
-          creaElementi(r)
+    cardsIt => {
+      console.log(cardsIt)
+      axios.get(`https://api.scryfall.com/cards/${cardsIt[0].data.set}/${cardsIt[0].data.collector_number}`).then(
+        cardsEng => {
+          console.log(cardsEng)
+          const carta = document.querySelector("#immagine-carta")
+          carta.style.backgroundImage = `url(${cardsEng.data.image_uris.art_crop})`;
+
+          //aggiungi bottoni
+          const buttons = creaBottoni()
+          const container = document.getElementById('container-bottoni')
+          const shuffledCardsIt = shuffleArray(cardsIt)
+          buttons.forEach((btn, i) => {
+            btn.textContent = shuffledCardsIt[i].data.printed_name
+            container.appendChild(btn)
+          })
         }
       )
       
     }
-  ) 
-}
-
-function creaElementi(r) {
-  const container = document.getElementById('container-bottoni')
-  for (let i = 0; i < 4; i++) {
-    const node = document.createElement('button')
-    node.classList.add('bg-blue-500', 'hover:bg-blue-400', 'text-white', 'font-bold', 'py-2', 'px-4', 'rounded', 'w-auto')
-    node.textContent = r[i].data.printed_name
-    container.appendChild(node)
-  }
-}
+  )
